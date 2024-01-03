e9fd8e99752acbbf54380321543ccc572e961bd6
deboravpereira <100171504+deboravpereira@users.noreply.github.com>
2024-01-03 14:17:54 -0300

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,61 +1,43 @@
 $(document).ready(function(){
-
     console.log('Ready')
 
-    //  Busque a data atual e atualize-a no DOM
+    let date_time = new Date()
+    let current_date = date_time.toLocaleDateString()
 
+    $('#date').text("Data: " + current_date)
 
+    $('#button').click(function(){
 
+        let review = $('#text').val()
+        console.log(review)
 
-    // Escreva um evento, quando o botão Enviar for clicado
-    $('').click(function(){
+        let input_data = {'customer_review' : review}
+        console.log(input_data)
 
-        //  Obtenha o valor do texto da área de texto usando o método 'val()'
-        let text_value = $('').val()
-
-        //  Converta-o em um objeto JS.
-        //  Forneça uma "chave" aqui e escreva o mesmo no arquivo app.py também para extrair dados
-        let input_text = {'' : text_value}
-        console.log(input_text)
-
-        //  requisição ajax
         $.ajax({
 
-            //  tipo da requisição web
-            type : '',
-
-            //  dados a serem enviados no formato JSON
-            data : JSON.stringify(),
-
-            //  o tipo de resposta esperado é json
+            url : "/predict",
+            type : "POST",
+            data : JSON.stringify(input_data),
             dataType : 'json',
-
-            //  contentType
             contentType : 'application/json',
-
-            //  se tudo funcionar, execute esta função
             success : function(result){
+                let prediction = result.prediction
+                let emoji_url = result.url
+                console.log(emoji_url)
 
-                // extraia previsão e a URL do emoticon do resultado
+                $('#sentiment').text(prediction)
+                $('#sentiment').show()
 
-
-                //  atualize os elementos DOM
-
-
-                //  exiba-os
-
+                $('#emoji').attr('src' , emoji_url)
+                $('#emoji').show()
             },
-
-            //  se houver algum erro, execute esta função
             error : function(result){
-
                 console.log(result)
             }
         })
 
-
-        //  limpando a caixa de texto após cada pressionamento de botão
         $('#text').val("")
-    })
         
+    })
 })
